800fa032f0cf08363a8664c408e3d19081013087
Jacob MacKinnon <tartvox@gmail.com>
2021-10-04 23:42:41 -0300

    Checkpoint1
    Creates a constant holding any temperature in Celsius.
    
    Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
    
    Prints the result for the user, showing both the Celsius and Fahrenheit values

--- Checkpoint1.playground/Contents.swift
+++ Checkpoint1.playground/Contents.swift
@@ -1,3 +1,7 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let celsius = 20
+
+let fahrenheit = celsius * 9 / 5 + 32
+
+print("The temp in celsius is: \(celsius)\nThe temp in fahrenheit is: \(fahrenheit)")
